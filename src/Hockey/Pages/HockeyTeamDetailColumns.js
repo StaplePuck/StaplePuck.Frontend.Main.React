7e27441017f1d583151c0721d697080fec2e0697
Jacob Distel <jacob.distel@gmail.com>
2019-04-24 19:33:29 -0400

    Added player scores

--- src/Hockey/Pages/HockeyTeamDetailColumns.js
+++ src/Hockey/Pages/HockeyTeamDetailColumns.js
@@ -23,5 +23,51 @@ export const HockeyTeamDetailsColumns = [
             background:
                 "linear-gradient(to bottom, rgba(255,214,94,1) 0%,rgba(254,191,4,1) 100%)"
         }
+    },
+    {
+        Header: "Total Points",
+        accessor: "playerCalculatedScore.score",
+        Cell: props => (
+            <span>
+                <div className="rt-mobileHeader">
+                    Total Points:
+                </div>
+                {props.value}
+            </span>
+        ),
+        style: { textAlign: "center" },
+        headerStyle: {
+            color: "#000",
+            background: "rgb(255,214,94)",
+            background:
+                "-moz-linear-gradient(top, rgba(255,214,94,1) 0%, rgba(254,191,4,1) 100%)",
+            background:
+                "-webkit-linear-gradient(top, rgba(255,214,94,1) 0%,rgba(254,191,4,1) 100%)",
+            background:
+                "linear-gradient(to bottom, rgba(255,214,94,1) 0%,rgba(254,191,4,1) 100%)"
+        }
+    },
+    {
+        Header: "Todays Points",
+        accessor: "playerCalculatedScore.todaysScore",
+        Cell: props => (
+            <span>
+                <div className="rt-mobileHeader">
+                    Todays Points:
+                </div>
+                {props.value}
+            </span>
+        ),
+        style: { textAlign: "center" },
+        headerStyle: {
+            color: "#000",
+            background: "rgb(255,214,94)",
+            background:
+                "-moz-linear-gradient(top, rgba(255,214,94,1) 0%, rgba(254,191,4,1) 100%)",
+            background:
+                "-webkit-linear-gradient(top, rgba(255,214,94,1) 0%,rgba(254,191,4,1) 100%)",
+            background:
+                "linear-gradient(to bottom, rgba(255,214,94,1) 0%,rgba(254,191,4,1) 100%)"
+        }
     }
 ];
